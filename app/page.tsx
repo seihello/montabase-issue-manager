b2727ac4932249cd9d3ac8ac8ffef9ad5fa64715
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-09-27 17:08:38 -0700

    feat: fetch singed-in user info

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 "use client";
 import GoogleSignInButton from "@/components/google-sign-in-button";
 import IssueOverview from "@/components/issue-overview";
+import createClient from "@/lib/supabase/client";
 import getAllIssues from "@/lib/supabase/get-all-issues";
 import { Issue } from "@/lib/types/issue.type";
 import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [user, setUser] = useState<any>();
   const [issues, setIssues] = useState<Issue[]>([]);
 
+  useEffect(() => {
+    const fetchUser = async () => {
+      const supabase = createClient();
+      const { data, error } = await supabase.auth.getSession();
+      console.log("user", data.session?.user);
+      setUser(user);
+    };
+    fetchUser();
+  }, []);
+  
   useEffect(() => {
     const fetchIssues = async () => {
       const issues = await getAllIssues();
