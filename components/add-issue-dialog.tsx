1e0d921f318055114da94eae20bd3a75c4342c94
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-09-27 21:19:21 -0700

    feat: add form of adding a new issue

---
+++ components/add-issue-dialog.tsx
@@ -0,0 +1,99 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { IconPencilPlus } from "@tabler/icons-react";
+
+import { useToast } from "@/components/hooks/use-toast";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const schema = z.object({
+  title: z.string().min(1, { message: "Title is missing" }),
+  description: z.string(),
+});
+
+export default function AddIssueDialog() {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
+
+  const { toast } = useToast();
+
+  async function onSubmit(values: z.infer<typeof schema>) {}
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="ghost" className="size-8 rounded-lg p-0 shadow-md">
+          <IconPencilPlus />
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <div className="h-4 w-4"></div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-4"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field, fieldState }) => (
+                <FormItem>
+                  {/* <FormLabel>Issue Title</FormLabel> */}
+                  <FormControl>
+                    <Input
+                      autoFocus
+                      placeholder="Issue title"
+                      className={`border-none focus-visible:ring-0 focus-visible:ring-transparent ${fieldState.invalid ? "border-destructive" : ""}`}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field, fieldState }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Add description here"
+                      className={`border-none focus-visible:ring-0 focus-visible:ring-transparent ${fieldState.invalid ? "border-destructive" : ""}`}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              className="w-full"
+              type="submit"
+              disabled={!form.formState.isValid}
+            >
+              Add
+            </Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,14 @@
-import GoogleSignInButton from "./google-sign-in-button";
+import AddIssueDialog from "@/components/add-issue-dialog";
+import GoogleSignInButton from "@/components/google-sign-in-button";
 
 export default function Sidebar() {
   return (
     <aside>
       <div className="h-screen w-56"></div>
-      <nav className="p-2 fixed left-0 top-0 flex h-screen w-56 flex-col border-r bg-white">
-        <div className="flex-1">This is sidebar</div>
+      <nav className="fixed left-0 top-0 flex h-screen w-56 flex-col items-stretch border-r bg-white p-2">
+        <div className="flex flex-1 flex-col items-end">
+          <AddIssueDialog />
+        </div>
         <GoogleSignInButton />
       </nav>
     </aside>
