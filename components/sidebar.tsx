57ae77839f32cdbfe8478912fff6f68fad744e4e
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-09-27 17:27:20 -0700

    style: make the sidebar narrow

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,8 +3,8 @@ import GoogleSignInButton from "./google-sign-in-button";
 export default function Sidebar() {
   return (
     <aside>
-      <div className="h-screen w-64"></div>
-      <nav className="p-2 fixed left-0 top-0 flex h-screen w-64 flex-col border-r bg-white">
+      <div className="h-screen w-56"></div>
+      <nav className="p-2 fixed left-0 top-0 flex h-screen w-56 flex-col border-r bg-white">
         <div className="flex-1">This is sidebar</div>
         <GoogleSignInButton />
       </nav>
