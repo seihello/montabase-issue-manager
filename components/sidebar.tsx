702ac2d2478518bd8040532344b10a34cc9551c7
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-10-03 14:49:42 -0700

    feat: add a company logo

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -16,8 +16,20 @@ export default function Sidebar({ isLoadingUser }: Props) {
     <aside>
       <div className="h-screen w-48"></div>
       <nav className="fixed left-0 top-0 flex h-screen w-48 flex-col items-stretch border-r bg-white p-2">
-        <div className="flex flex-1 flex-col items-end">
-          <AddIssueDialog />
+        <div className="flex flex-1 flex-col">
+          <div className="flex justify-between">
+            <a href="/" className="flex items-center text-lg font-bold">
+              <Image
+                src="/img/snow-capped-mountain.png"
+                alt="profile_avatar"
+                width={32}
+                height={32}
+                className="mb-1 rounded-full"
+              />
+              Montabase
+            </a>
+            <AddIssueDialog />
+          </div>
         </div>
         <div className="flex flex-col items-center gap-y-1">
           {user && (
