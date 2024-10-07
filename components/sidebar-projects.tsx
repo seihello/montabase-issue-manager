a32b66b655402729f9e88e884c5bc1f568b4fe5a
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-10-06 21:53:04 -0700

    feat: click a plus button to edit the name of a new project

--- components/sidebar-projects.tsx
+++ components/sidebar-projects.tsx
@@ -1,15 +1,11 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import getAllProjects from "@/lib/supabase/get-all-projects";
 import { projectsState } from "@/states/projects-state";
 import { userState } from "@/states/user-state";
+import { IconPlus } from "@tabler/icons-react";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 
 export default function SidebarProjects() {
@@ -18,6 +14,11 @@ export default function SidebarProjects() {
   const user = useRecoilValue(userState);
   const projects = useRecoilValue(projectsState);
   const setProjects = useSetRecoilState(projectsState);
+
+  const [newProjectTitle, setNewProjectTitle] = useState<string | null>(null);
+  const [isProjectTitleSelected, setIsProjectTitleSelected] =
+    useState<boolean>(false);
+  const newProjectTitleInput = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -31,29 +32,51 @@ export default function SidebarProjects() {
     };
     fetchProjects();
   }, [user, setProjects]);
+
+  const addProject = async () => {
+    // const newProject = setProjects((oldProjects) => [...oldProjects, {}]);
+    setNewProjectTitle("New Project");
+  };
+
+  useEffect(() => {
+    if (newProjectTitleInput.current && !isProjectTitleSelected) {
+      newProjectTitleInput.current.select();
+      setIsProjectTitleSelected(true);
+    }
+  }, [newProjectTitle, isProjectTitleSelected]);
+
   return (
-    <Accordion
-      type="single"
-      collapsible
-      className="w-full"
-      defaultValue="projects"
-    >
-      <AccordionItem value="projects" className="border-none">
-        <AccordionTrigger className="text-xs font-semibold text-gray-700 hover:no-underline">
-          Your projects
-        </AccordionTrigger>
-        {projects.map((project, index) => (
-          <AccordionContent key={index} asChild>
-            <Button
-              variant="ghost"
-              className="h-8 w-full justify-start"
-              onClick={() => router.push(`/projects/${project.id}`)}
-            >
-              <span className="truncate">{project.title}</span>
-            </Button>
-          </AccordionContent>
-        ))}
-      </AccordionItem>
-    </Accordion>
+    <div className="relative mb-2 mt-4 border-none">
+      <div className="mb-2 text-xs font-semibold text-gray-700 hover:no-underline">
+        <span>Your projects</span>
+      </div>
+      <Button
+        variant="ghost"
+        onClick={addProject}
+        className="absolute -top-[2px] right-0 h-auto w-auto p-1"
+      >
+        <IconPlus size={12} />
+      </Button>
+      {projects.map((project, index) => (
+        <div key={index}>
+          <Button
+            variant="ghost"
+            className="h-8 w-full justify-start"
+            onClick={() => router.push(`/projects/${project.id}`)}
+          >
+            <span className="truncate">{project.title}</span>
+          </Button>
+        </div>
+      ))}
+
+      {newProjectTitle !== null && (
+        <Input
+          ref={newProjectTitleInput}
+          className={`h-8 w-full justify-start`}
+          value={newProjectTitle}
+          onChange={(e) => setNewProjectTitle(e.target.value)}
+        />
+      )}
+    </div>
   );
 }
