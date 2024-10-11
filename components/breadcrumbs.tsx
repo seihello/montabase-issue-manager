2cec2926a222711bcaf78ab3a5caed1e22f892fe
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-10-10 19:03:21 -0700

    feat: add project link to the breadcrumbs

--- components/breadcrumbs.tsx
+++ components/breadcrumbs.tsx
@@ -1,4 +1,5 @@
 import { IconChevronRight } from "@tabler/icons-react";
+import { useRouter } from "next/navigation";
 
 type Props = {
   isAllProjects: boolean;
@@ -21,9 +22,22 @@ export default function Breadcrumbs({
     throw new Error("Provide the project data");
   if (!isAllIssues && (!issueId || !issueTitle))
     throw new Error("Provide the issue data");
+
+  const router = useRouter();
   return (
-    <div className="flex items-center gap-x-2 px-2">
-      <div>{isAllProjects ? "All Projects" : projectTitle}</div>
+    <div className="flex items-center gap-x-1 px-2">
+      <div
+        className="cursor-pointer hover:underline"
+        onClick={() => {
+          if (isAllProjects) {
+            router.push("/");
+          } else {
+            router.push(`/projects/${projectId}`);
+          }
+        }}
+      >
+        {isAllProjects ? "All Projects" : projectTitle}
+      </div>
       <IconChevronRight size={16} className="mb-[2px]" />
       <div>{isAllIssues ? "All Issues" : issueTitle}</div>
     </div>
