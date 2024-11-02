b9b33d78d71dec289c22f771f34a5dee40b8ca74
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-11-02 13:50:25 -0700

    style: add transition when the demo message is closed

--- components/demo-message.tsx
+++ components/demo-message.tsx
@@ -2,18 +2,40 @@ import { Button } from "@/components/ui/button";
 import useGoogleSignIn from "@/hooks/use-google-sign-in";
 import { isDemoMessageHiddenState } from "@/states/demo-state";
 import { IconX } from "@tabler/icons-react";
+import { useEffect, useRef, useState } from "react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 
 export default function DemoMessage() {
   const { signInWithGoogle } = useGoogleSignIn();
-  // const [isHidden, setIsHidden] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState(80);
   const isDemoMessageHidden = useRecoilValue(isDemoMessageHiddenState);
   const setIsDemoMessageHidden = useSetRecoilState(isDemoMessageHiddenState);
 
-  if (isDemoMessageHidden) return;
+  useEffect(() => {
+    const handleResize = () => {
+      if (ref.current) setHeight(ref.current.scrollHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Call handleResize initially in case the window size is already below 768px
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  console.log("height", height);
 
   return (
-    <div className="flex items-center justify-between border-y-[1px] border-yellow-300 bg-yellow-50 pl-4 pr-1 text-yellow-700">
+    <div
+      ref={ref}
+      className="flex items-center justify-between overflow-hidden border-yellow-300 bg-yellow-50 pl-4 pr-1 text-yellow-700"
+      style={{
+        maxHeight: isDemoMessageHidden ? 0 : height,
+        transition: "max-height 0.5s",
+      }}
+    >
       <p>
         This is a demo mode. Feel free to add projects and issues. Note that all
         your changes will be lost once you leave the page. To save your items,{" "}
