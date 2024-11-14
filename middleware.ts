3967f69aef5b8dad58d0bea6d8cfc18ed479a78d
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-11-13 18:46:05 -0800

    feat: redirect to seisuke.info for the time being

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,18 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
+  if (request.nextUrl.hostname !== "montabase.seisuke.info" && request.nextUrl.hostname !== "localhost") {
+    return NextResponse.redirect(
+      new URL("https://montabase.seisuke.info/project/all"),
+    );
+  }
+
   let response = NextResponse.next({
     request: {
       headers: request.headers,
     },
-  })
+  });
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL!,
@@ -14,39 +20,39 @@ export async function middleware(request: NextRequest) {
     {
       cookies: {
         get(name: string) {
-          return request.cookies.get(name)?.value
+          return request.cookies.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
           request.cookies.set({
             name,
             value,
             ...options,
-          })
+          });
           response = NextResponse.next({
             request: {
               headers: request.headers,
             },
-          })
+          });
         },
         remove(name: string, options: CookieOptions) {
           request.cookies.set({
             name,
-            value: '',
+            value: "",
             ...options,
-          })
+          });
           response = NextResponse.next({
             request: {
               headers: request.headers,
             },
-          })
+          });
         },
       },
-    }
-  )
+    },
+  );
 
-  await supabase.auth.getSession()
+  await supabase.auth.getSession();
 
-  return response
+  return response;
 }
 
 export const config = {
@@ -58,6 +64,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    "/((?!_next/static|_next/image|favicon.ico).*)",
   ],
-}
+};
